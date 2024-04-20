29ca2643767f3a765e5760e5425fd2fd970f9afa
Dpastor1 <dpastor@colostate.edu>
2024-04-20 14:10:44 -0600

    Fixed API calls

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,4 +42,5 @@ dependencies {
 
     implementation ("com.google.android.gms:play-services-vision:19.0.0")
     implementation ("com.theartofdev.edmodo:android-image-cropper:2.8.+")
+    implementation ("com.android.volley:volley:1.2.1")
 }
